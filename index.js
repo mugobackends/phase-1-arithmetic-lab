5b1fc1a021f8c10ac5371a334d90d1a766aabf7a
Lee <lee.thuku@student.moringaschool.com>
2025-03-17 11:20:27 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// 1. Multiply num1 and num2 to get 62
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+// 2. Generate a random integer greater than 0
+let random = Math.floor(Math.random() * 100) + 1;
+
+// 3. Calculate the remainder of dividing num3 by num4 to get 4
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+// 4. Find the highest number in a set, which should be 20
+let max = Math.max(10, 15, 20, 5);
